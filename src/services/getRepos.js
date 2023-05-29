ddf3ff8c041a4b65c89208aa90b81bcaf8a18c08
Kevin Cain <kevin_cain@hotmail.co.uk>
2023-05-29 19:47:24 +0100

    add getRepos service

---
+++ src/services/getRepos.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const getRepos = query => {
+    if (!query) {
+      return Promise.resolve([]);
+    }
+  
+    return axios
+      .get(
+        `https://api.github.com/search/repositories?q=${query}`
+      )
+      .then(function(response) {
+        return response.data.results.map(repos => repos);
+      })
+      .catch(function(err) {
+        console.error(err);
+      });
+  };
+  
+  export default getRepos;
